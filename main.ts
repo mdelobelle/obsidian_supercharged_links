9a97469f526b0327674874f36cb4983c3c873c7e
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-04-12 15:25:47 +0200

    Make SL work with `Another Quick Switcher` again
    fixes #100

--- main.ts
+++ main.ts
@@ -144,7 +144,7 @@ export default class SuperchargedLinks extends Plugin {
 							(n.className.includes('modal-container') && plugin.settings.enableQuickSwitcher
 								// @ts-ignore
 								|| n.className.includes('suggestion-container') && plugin.settings.enableSuggestor)) {
-							let selector = ".suggestion-item, .suggestion-note, .another-quick-switcher__item__file";
+							let selector = ".suggestion-item, .suggestion-note, .another-quick-switcher__item__title";
 							// @ts-ignore
 							if (n.className.includes('suggestion-container')) {
 								selector = ".suggestion-content, .suggestion-note";
