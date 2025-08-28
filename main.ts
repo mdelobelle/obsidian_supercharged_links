f886ff8df520cbddcabe1ade8b50466cb3c7e446
flowing-abyss <flowing.abyss@gmail.com>
2025-08-28 18:19:28 +0700

    Support for Similar Notes

--- main.ts
+++ main.ts
@@ -100,6 +100,9 @@ export default class SuperchargedLinks extends Plugin {
 			plugin.registerViewType('markdown', plugin, 'div.bases-table-cell  .internal-link');
 		}
 
+		if (plugin.app?.plugins?.plugins?.['similar-notes']) {
+			plugin.registerViewType('markdown', plugin, '.similar-notes-pane .tree-item-inner')
+		}
 		// If backlinks in editor is on
 		// @ts-ignore
 		if (plugin.app?.internalPlugins?.plugins?.backlink?.enabled && plugin.app?.internalPlugins?.plugins?.backlink?.instance?.options?.backlinkInDocument) {
